5fae0ced6541334a1291c85a006425efb8ed829b
GurnoorSingh17 <gokugurnoor@gmail.com>
2025-09-10 01:39:38 +0530

    changes

--- first project/first project/src/test/ThreadsBencho.java
+++ first project/first project/src/test/ThreadsBencho.java
@@ -5,9 +5,9 @@ public class ThreadsBencho {
 
     public static void main(String[] args){
 
-        String Str= "balle balle";
+     
 
-        System.out.println(Str);
+       
 
         
     }
