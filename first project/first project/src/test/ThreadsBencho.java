40c6c9db1d4988ad855e02f8043ecca9ed2dbd49
GurnoorSingh17 <gokugurnoor@gmail.com>
2025-09-10 02:09:11 +0530

    done

--- first project/first project/src/test/ThreadsBencho.java
+++ first project/first project/src/test/ThreadsBencho.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 
 public class ThreadsBencho {
 
@@ -7,13 +6,7 @@ public class ThreadsBencho {
 
     public static void main(String[] args){
 
-        Scanner Scn = new Scanner(System.in);
-        System.out.println("Enter a Double:-");
-        double IntInput = Scn.nextDouble();
-        System.out.println("Ans:- ");
-        System.out.println(IntInput/10 + 30 - IntInput/53);
-       
-        Scn.close();
+        
         
     }
 }
